b19c2aabb155ba09151a0d2d92baac75a6330740
onsantanu <onsantanu.co.in@gmail.com>
2021-07-01 18:17:23 +0530

    first commit

---
+++ json.js
@@ -0,0 +1,62 @@
+$( document ).ready(function() {
+    document.addEventListener('contextmenu', event => event.preventDefault());
+    $('body').keydown(function(e) {
+        if(e.which==123){
+            e.preventDefault();
+        }
+        if(e.ctrlKey && e.shiftKey && e.which == 73){
+            e.preventDefault();
+        }
+        if(e.ctrlKey && e.shiftKey && e.which == 75){
+            e.preventDefault();
+        }
+        if(e.ctrlKey && e.shiftKey && e.which == 67){
+            e.preventDefault();
+        }
+        if(e.ctrlKey && e.shiftKey && e.which == 74){
+            e.preventDefault();
+        }
+    });
+	!function() {
+        function detectDevTool(allow) {
+            if(isNaN(+allow)) allow = 100;
+            var start = +new Date();
+            debugger;
+            var end = +new Date();
+            if(isNaN(start) || isNaN(end) || end - start > allow) {
+                console.log('DEVTOOLS detected '+allow);
+            }
+        }
+        if(window.attachEvent) {
+            if (document.readyState === "complete" || document.readyState === "interactive") {
+                detectDevTool();
+              window.attachEvent('onresize', detectDevTool);
+              window.attachEvent('onmousemove', detectDevTool);
+              window.attachEvent('onfocus', detectDevTool);
+              window.attachEvent('onblur', detectDevTool);
+            } else {
+                setTimeout(argument.callee, 0);
+            }
+        } else {
+            window.addEventListener('load', detectDevTool);
+            window.addEventListener('resize', detectDevTool);
+            window.addEventListener('mousemove', detectDevTool);
+            window.addEventListener('focus', detectDevTool);
+            window.addEventListener('blur', detectDevTool);
+        }
+    }();
+});
+function jsonView() {
+	try {
+      var input = eval('(' + $('#json-input').val() + ')');
+    }
+    catch (error) {
+      return alert("Cannot eval JSON: " + error);
+    }
+	$('#json-renderer').jsonViewer(input, {collapsed: false, withQuotes: false, withLinks: true});
+}
+
+function onclear() {
+	$('#json-input').val('')
+	$('#json-renderer').empty()
+}
